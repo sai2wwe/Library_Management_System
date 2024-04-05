331632863215aca16a2c1de600e26cede886d725
♥🥀دانية🥀♠ <161941402+7-danny-7@users.noreply.github.com>
2024-04-05 23:42:20 +0400

    Update hero.jsx

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -3,7 +3,7 @@ export default function Hero() {
     return (
         <div className="grid max-w-screen-xl px-4 py-8 mx-auto gap-4 lg:gap-8 xl:gap-0 lg:py-16 grid-cols-3 lg:grid-cols-12 h-full">
             <div className="mr-auto place-self-center col-span-2 lg:col-span-7 px-6">
-                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white font-serif"><i><u>BIBILIOSYNC</u></i></h1>
+                <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white font-serif"><i><u>BIBLIOTHÈQUE</u></i></h1>
                 <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"><i>Online Library Management System</i></p>
                 <Link to="/books" className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                     View Books
