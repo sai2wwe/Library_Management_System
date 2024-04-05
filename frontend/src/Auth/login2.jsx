03724229b3bc043c31cc484b6051f5c013503399
♥🥀دانية🥀♠ <161941402+7-danny-7@users.noreply.github.com>
2024-04-05 22:41:12 +0400

    Update login2.jsx

--- frontend/src/Auth/login2.jsx
+++ frontend/src/Auth/login2.jsx
@@ -149,7 +149,7 @@ export default function Login() {
                       className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                       disabled={loading || disable}
                     >
-                      Sign in
+                      Login
                     </button>
                   </div>
                 </form>
